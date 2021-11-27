2b6f3e9fe44913a9735019b15c1dda80c1379b7b
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2021-11-27 14:25:23 +0000

    Made syntax into mutually inductive type

--- LogicalRefinement.lean
+++ LogicalRefinement.lean
@@ -1,53 +1,56 @@
-inductive Untyped where
-  -- Types
-  | nat
-  | pi (A: Untyped) (B: Untyped)
-  | sigma (A: Untyped) (B: Untyped)
-  | coprod (A: Untyped) (B: Untyped)
-  | set (A: Untyped) (B: Untyped)
-  | assume (φ: Untyped) (A: Untyped)
-  | intersect (A: Untyped) (B: Untyped)
-  | union (A: Untyped) (B: Untyped)
+mutual
 
-  -- Propositions
+inductive Ty where
+  | nat
+  | pi (A: Ty) (B: Ty)
+  | sigma (A: Ty) (B: Ty)
+  | coprod (A: Ty) (B: Ty)
+  | set (A: Ty) (φ: Pr)
+  | assume (φ: Pr) (A: Ty)
+  | intersect (A: Ty) (B: Ty)
+  | union (A: Ty) (B: Ty)
+
+inductive Pr where
   | top
   | bot
-  | and (φ: Untyped) (ψ: Untyped)
-  | or (φ: Untyped) (ψ: Untyped)
-  | implies (φ: Untyped) (ψ: Untyped)
-  | forall_ (A: Untyped) (φ: Untyped)
-  | exists_ (A: Untyped) (φ: Untyped)
-  | eq (A: Untyped) (e: Untyped) (e': Untyped)
+  | and (φ: Pr) (ψ: Pr)
+  | or (φ: Pr) (ψ: Pr)
+  | implies (φ: Pr) (ψ: Pr)
+  | forall_ (A: Ty) (φ: Pr)
+  | exists_ (A: Ty) (φ: Pr)
+  | eq (A: Ty) (e: Term) (e': Term)
 
-  -- Terms
+inductive Term where
   | var (n: Nat)
-  | lam (A: Untyped) (e: Untyped)
-  | app (l: Untyped) (r: Untyped)
-  | pair (l: Untyped) (r: Untyped)
-  | proj (b: Bool) (e: Untyped)
-  | inj (b: Bool) (e: Untyped)
-  | case (e: Untyped) (l: Untyped) (r: Untyped)
-  | mkset (e: Untyped) (p: Untyped)
-  | letset (e: Untyped)
-  | lam_pr (φ: Untyped) (e: Untyped)
-  | app_pr (e: Untyped) (p: Untyped)
-  | lam_irrel (A: Untyped) (e: Untyped)
-  | app_irrel (l: Untyped) (r: Untyped)
-  | repr (l: Untyped) (r: Untyped)
-  | let_repr (e: Untyped)
+  | lam (A: Ty) (e: Term)
+  | app (l: Term) (r: Term)
+  | pair (l: Term) (r: Term)
+  | proj (b: Bool) (e: Term)
+  | inj (b: Bool) (e: Term)
+  | case (e: Term) (l: Term) (r: Term)
+  | mkset (e: Term) (p: Proof)
+  | letset (e: Term)
+  | lam_pr (φ: Proof) (e: Term)
+  | app_pr (e: Term) (p: Proof)
+  | lam_irrel (A: Ty) (e: Term)
+  | app_irrel (l: Term) (r: Term)
+  | repr (l: Term) (r: Term)
+  | let_repr (e: Term)
 
-  -- Proofs
+inductive Proof where
   | nil
-  | abort (p: Untyped)
-  | conj (l: Untyped) (r: Untyped)
-  | proj_conj (b: Bool) (p: Untyped)
-  | disj (b: Bool) (p: Untyped)
-  | case_disj (p: Untyped) (l: Untyped) (r: Untyped)
-  | lam_imp (φ: Untyped) (p: Untyped)
-  | ponens (l: Untyped) (r: Untyped)
-  | general (A: Untyped) (p: Untyped)
-  | inst (p: Untyped) (e: Untyped)
-  | witness (e: Untyped) (p: Untyped)
-  | let_wit (p: Untyped)
-  | refl (e: Untyped)
+  | abort (p: Proof)
+  | conj (l: Proof) (r: Proof)
+  | proj (b: Bool) (p: Proof)
+  | disj (b: Bool) (p: Proof)
+  | case (p: Proof) (l: Proof) (r: Proof)
+  | lam (φ: Proof) (p: Proof)
+  | app (l: Proof) (r: Proof)
+  | general (A: Ty) (p: Proof)
+  | inst (p: Proof) (e: Term)
+  | witness (e: Term) (p: Proof)
+  | let_wit (p: Proof)
+  | refl (e: Term)
   --TODO: equality axioms...
+
+end
