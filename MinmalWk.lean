0b60d3d813318a02be85c9f9d60504c4cf53e9da
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2021-12-01 19:55:49 +0000

    Var functions

--- MinmalWk.lean
+++ MinmalWk.lean
@@ -10,7 +10,9 @@ inductive RawWk: Type 0 where
     | RawWk.lift ρ, RawWk.step σ => RawWk.step (raw_wk_comp ρ σ)
     | RawWk.lift ρ, RawWk.lift σ => RawWk.lift (raw_wk_comp ρ σ)
 
-theorem raw_wk_comp_id_left_id {ρ: RawWk}: raw_wk_comp RawWk.id ρ = ρ := rfl
+@[simp] def RawWk.comp := raw_wk_comp
+
+theorem raw_wk_comp_id_left_id {ρ: RawWk}: RawWk.comp RawWk.id ρ = ρ := rfl
 
 @[simp] theorem raw_wk_comp_id_right_id: {ρ: RawWk} -> raw_wk_comp ρ RawWk.id = ρ
   | RawWk.id => rfl
@@ -23,7 +25,9 @@ theorem raw_wk_comp_id_left_id {ρ: RawWk}: raw_wk_comp RawWk.id ρ = ρ := rfl
 @[simp] theorem raw_wk_comp_assoc: {ρ σ τ: RawWk} -> 
   raw_wk_comp (raw_wk_comp ρ σ) τ = raw_wk_comp ρ (raw_wk_comp σ τ)
   | RawWk.id, _, _ => rfl
-  | _, RawWk.id, _ => by simp
+  | _, RawWk.id, _ => by {
+    simp
+  }
   | _, _, RawWk.id => by simp
   | RawWk.step ρ, _, _ => by {
     simp
@@ -48,6 +52,8 @@ theorem raw_wk_comp_id_left_id {ρ: RawWk}: raw_wk_comp RawWk.id ρ = ρ := rfl
   | RawWk.lift ρ, 0 => 0
   | RawWk.lift ρ, (n + 1) => (raw_wk_var ρ n) + 1
 
+@[simp] def RawWk.var := raw_wk_var
+  
 @[simp] theorem raw_wk_var_comp: (ρ σ: RawWk) -> (n: Nat) ->
   raw_wk_var ρ (raw_wk_var σ n) = raw_wk_var (raw_wk_comp ρ σ) n
   | RawWk.id, _, _ => rfl
@@ -99,8 +105,18 @@ structure Wk (m n: Nat) := (val: RawWk) (p: wk_maps m n val)
 @[simp] def Wk.comp {m n l: Nat} (ρ: Wk m n) (σ: Wk n l): Wk m l :=
   Wk.mk (raw_wk_comp ρ.val σ.val) (wk_maps_comp ρ.p σ.p)
 
-theorem wk_comp_assoc {m n l p: Nat} (ρ: Wk m n) (σ: Wk n l) (τ: Wk l p):
+@[simp] theorem wk_comp_assoc {m n l p: Nat} (ρ: Wk m n) (σ: Wk n l) (τ: Wk l p):
   Wk.comp ρ (Wk.comp σ τ) = Wk.comp (Wk.comp ρ σ) τ := by {
+  simp
+}
+
+@[simp] def Wk.raw_var {m n: Nat} (ρ: Wk m n): Nat -> Nat := RawWk.var ρ.val
+
+@[simp] def Wk.var {m n: Nat} (ρ: Wk m n): Fin n -> Fin m
+  | Fin.mk v p => Fin.mk (Wk.raw_var ρ v) (ρ.p v p)
+
+@[simp] theorem wk_var_comp {m n l: Nat} (ρ: Wk m n) (σ: Wk n l) (v: Fin l):
+  Wk.var ρ (Wk.var σ v) = Wk.var (Wk.comp ρ σ) v := by {
   simp
 }
 
