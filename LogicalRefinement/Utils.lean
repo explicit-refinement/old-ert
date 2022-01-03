607c7906fc3bdb5ebcec003bcedcbeb8e07d9f08
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-01-03 23:55:29 +0000

    Tap

--- LogicalRefinement/Utils.lean
+++ LogicalRefinement/Utils.lean
@@ -1,6 +1,6 @@
 import Init.Data.Nat
 
-def eq_zero_is_le_zero: (m ≤ 0) = (m = 0) := propext (
+def Nat.eq_zero_is_le_zero: (m ≤ 0) = (m = 0) := propext (
   Iff.intro
   Nat.eq_zero_of_le_zero
   (by { sorry })
@@ -8,7 +8,7 @@ def eq_zero_is_le_zero: (m ≤ 0) = (m = 0) := propext (
 
 def max_zero_left: (m: Nat) -> Nat.max m 0 = m
   | 0 => rfl
-  | Nat.succ n => by { simp only [Nat.max, eq_zero_is_le_zero]; simp }
+  | Nat.succ n => by { simp only [Nat.max, Nat.eq_zero_is_le_zero]; simp }
 
 def max_zero_right: (m: Nat) -> Nat.max 0 m = m
   | 0 => rfl
