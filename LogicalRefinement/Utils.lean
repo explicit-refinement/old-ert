4b2fa48a0eca1c28d221fe82a8e2057cd91b6a8f
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-01-03 23:56:51 +0000

    Better simp

--- LogicalRefinement/Utils.lean
+++ LogicalRefinement/Utils.lean
@@ -5,8 +5,8 @@ def Nat.eq_zero_is_le_zero: (m ≤ 0) = (m = 0) :=
 
 def max_zero_left: (m: Nat) -> Nat.max m 0 = m
   | 0 => rfl
-  | Nat.succ n => by { simp only [Nat.max, Nat.eq_zero_is_le_zero]; simp }
+  | Nat.succ n => by { simp [Nat.max, Nat.eq_zero_is_le_zero] }
 
 def max_zero_right: (m: Nat) -> Nat.max 0 m = m
   | 0 => rfl
-  | Nat.succ n => by { simp only [Nat.max, Nat.zero_le]; simp }
+  | Nat.succ n => by { simp [Nat.max, Nat.zero_le] }
