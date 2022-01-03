18745c6ccf7e088797ad963a02655fedd0fa7cef
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-01-03 23:52:40 +0000

    Better structure

--- LogicalRefinement/Utils.lean
+++ LogicalRefinement/Utils.lean
@@ -1,11 +1,10 @@
 import Init.Data.Nat
 
-def eq_zero_is_le_zero: (m ≤ 0) = (m = 0) := by {
-  apply propext;
-  apply Iff.intro;
-  sorry
-  sorry
-}
+def eq_zero_is_le_zero: (m ≤ 0) = (m = 0) := propext (
+  Iff.intro
+  (by { sorry })
+  (by { sorry })
+)
 
 def max_zero_left: (m: Nat) -> Nat.max m 0 = m
   | 0 => rfl
