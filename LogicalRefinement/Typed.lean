9624b455ccae92d48a6f3861134565d7170847d3
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-02-12 21:56:18 +0000

    Hmm... single hastype?

--- LogicalRefinement/Typed.lean
+++ LogicalRefinement/Typed.lean
@@ -6,12 +6,24 @@ inductive Annot
   | prop
   | expr (A: RawUntyped)
 
-structure Hyp := (annot: RawUntyped) (comp: Bool)
+@[simp]
+def Annot.wk1: Annot -> Annot
+  | type => type
+  | prop => prop
+  | expr A => expr (RawUntyped.wk1 A)
 
-def Hyp.upgrade (h: Hyp): Hyp := Hyp.mk (h.annot) true
+inductive Hyp
+  | val (A: RawUntyped)
+  | ghost (A: RawUntyped)
+
+@[simp]
+def Hyp.upgrade: Hyp -> Hyp
+  | val A => val A
+  | ghost A => val A
 
 def RawContext := List Hyp
 
+@[simp]
 def RawContext.upgrade: RawContext -> RawContext
   | [] => []
   | h::hs => (h.upgrade)::hs
@@ -26,80 +38,82 @@ def constAnnot: UntypedKind [] -> Annot
   | UntypedKind.nat _ => expr nats
   | UntypedKind.nil => expr top
 
-inductive RawHasType: RawContext -> RawUntyped -> Annot -> Prop
+inductive HasType: RawContext -> RawUntyped -> Annot -> Prop
   -- Variables
-  | var {Γ: RawContext} {n: Nat} {A: RawUntyped}:
-    Γ.get? n = some (Hyp.mk A true) ->
-    RawHasType Γ (var n) (expr (wkn n A))
-  
+  | var0 {Γ: RawContext} {A: RawUntyped}:
+    HasType Γ A type ->
+    HasType ((Hyp.val A)::Γ) (var 0) (expr A)
+
   -- Constants
-  | const {Γ: RawContext} {c: UntypedKind []}:
-    RawHasType Γ (const c) (constAnnot c)
+  | const0 {c: UntypedKind []}:
+    HasType [] (const c) (constAnnot c)
+  
+  -- Weakening
+  | wk_val {Γ a A B}:
+    HasType Γ a A -> HasType Γ B type 
+    -> HasType ((Hyp.val B)::Γ) (wk1 a) (wk1 A)
+  | wk_ghost {Γ a A B}:
+    HasType Γ a A -> HasType Γ B type 
+    -> HasType ((Hyp.ghost B)::Γ) (wk1 a) (wk1 A)
+  | wk_prop {Γ a A B}:
+    HasType Γ a A -> HasType Γ B prop 
+    -> HasType ((Hyp.val B)::Γ) (wk1 a) (wk1 A)
 
   -- Basic types
   | pi {Γ: RawContext} {A B: RawUntyped}:
-    RawHasType Γ A type -> RawHasType ((Hyp.mk A true)::Γ) B type ->
-    RawHasType Γ (pi A B) type
+    HasType Γ A type -> HasType ((Hyp.val (wk1 A))::Γ) B type ->
+    HasType Γ (pi A B) type
   | sigma {Γ: RawContext} {A B: RawUntyped}:
-    RawHasType Γ A type -> RawHasType ((Hyp.mk A true)::Γ) B type ->
-    RawHasType Γ (sigma A B) type
+    HasType Γ A type -> HasType ((Hyp.val (wk1 A))::Γ) B type ->
+    HasType Γ (sigma A B) type
   | coprod {Γ: RawContext} {A B: RawUntyped}:
-    RawHasType Γ A type -> RawHasType Γ B type ->
-    RawHasType Γ (coprod A B) type
+    HasType Γ A type -> HasType Γ B type ->
+    HasType Γ (coprod A B) type
   | set {Γ: RawContext} {A B: RawUntyped}:
-    RawHasType Γ A type -> RawHasType ((Hyp.mk A true)::Γ) B prop ->
-    RawHasType Γ (set A B) type
+    HasType Γ A type -> HasType ((Hyp.val (wk1 A))::Γ) B prop ->
+    HasType Γ (set A B) type
   | assume {Γ: RawContext} {φ A: RawUntyped}:
-    RawHasType Γ φ type  -> RawHasType Γ B type ->
-    RawHasType Γ (assume φ A) type
+    HasType Γ φ type  -> HasType Γ B type ->
+    HasType Γ (assume φ A) type
   | intersect {Γ: RawContext} {A B: RawUntyped}:
-    RawHasType Γ A type -> RawHasType ((Hyp.mk A false)::Γ) B prop ->
-    RawHasType Γ (intersect A B) type
+    HasType Γ A type -> HasType ((Hyp.val (wk1 A))::Γ) B prop ->
+    HasType Γ (intersect A B) type
   | union {Γ: RawContext} {A B: RawUntyped}:
-    RawHasType Γ A type -> RawHasType ((Hyp.mk A false)::Γ) B prop ->
-    RawHasType Γ (union A B) type
+    HasType Γ A type -> HasType ((Hyp.val (wk1 A))::Γ) B prop ->
+    HasType Γ (union A B) type
   
   -- Basic propositions
   | and {Γ: RawContext} {φ ψ: RawUntyped}:
-    RawHasType Γ φ prop -> RawHasType Γ ψ prop ->
-    RawHasType Γ (and φ ψ) prop
+    HasType Γ φ prop -> HasType Γ ψ prop ->
+    HasType Γ (and φ ψ) prop
   | or {Γ: RawContext} {φ ψ: RawUntyped}:
-    RawHasType Γ φ prop -> RawHasType Γ ψ prop ->
-    RawHasType Γ (or φ ψ) prop
+    HasType Γ φ prop -> HasType Γ ψ prop ->
+    HasType Γ (or φ ψ) prop
   | implies {Γ: RawContext} {φ ψ: RawUntyped}:
-    RawHasType Γ φ prop -> RawHasType Γ ψ prop ->
-    RawHasType Γ (implies φ ψ) prop
+    HasType Γ φ prop -> HasType Γ ψ prop ->
+    HasType Γ (implies φ ψ) prop
   | forall_ {Γ: RawContext} {A φ: RawUntyped}:
-    RawHasType Γ A type -> RawHasType ((Hyp.mk A false)::Γ) φ prop ->
-    RawHasType Γ (forall_ A φ) type
+    HasType Γ A type -> HasType ((Hyp.val (wk1 A))::Γ) φ prop ->
+    HasType Γ (forall_ A φ) type
   | exists_ {Γ: RawContext} {A φ: RawUntyped}:
-    RawHasType Γ A type -> RawHasType ((Hyp.mk A false)::Γ) φ prop ->
-    RawHasType Γ (exists_ A φ) type
+    HasType Γ A type -> HasType ((Hyp.val (wk1 A))::Γ) φ prop ->
+    HasType Γ (exists_ A φ) type
   | eq {Γ: RawContext} {A l r: RawUntyped}:
-    RawHasType Γ A type -> 
-    RawHasType Γ.upgrade l (expr A) -> RawHasType Γ.upgrade r (expr A) ->
-    RawHasType Γ (eq A l r) prop
+    HasType Γ A type -> 
+    HasType Γ.upgrade l (expr A) -> HasType Γ.upgrade r (expr A) ->
+    HasType Γ (eq A l r) prop
 
   -- Basic terms
-  --TODO: this
+  | lam {Γ: RawContext} {A s B: RawUntyped}:
+    HasType Γ A type -> HasType ((Hyp.val (wk1 A))::Γ) s (expr B) ->
+    HasType Γ (lam A s) (expr (pi A B))
 
   -- Basic proofs
   --TODO: this
 
-theorem RawHasType.upgrade (p: RawHasType Γ a A): RawHasType Γ.upgrade a A := by {
+def RawUntyped.arrow (A: RawUntyped) := pi A (wk1 A)
+
+theorem HasType.upgrade (p: HasType Γ a A): HasType Γ.upgrade a A := by {
   induction p with
-  | const => exact const
   | _ => sorry
 }
-
-inductive IsContext: RawContext -> Prop
-  | nil: IsContext []
-  | cons_typ (Γ: RawContext) (A: RawUntyped) (comp: Bool):
-    RawHasType Γ A type -> IsContext Γ -> IsContext ((Hyp.mk A comp)::Γ)
-  | cons_prop (Γ: RawContext) (A: RawUntyped):
-    RawHasType Γ A type -> IsContext Γ -> IsContext ((Hyp.mk A true)::Γ)
-
-structure Context := (val: RawContext) (p: IsContext val)
-
---TODO: think about this
-def HasType (Γ: Context) (a: RawUntyped) (A: Annot) := RawHasType Γ.val a A
