aead12f9f0bdb423c5b064c62dc0028d00b0f4dc
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-02-13 20:12:54 +0000

    More constructors...

--- LogicalRefinement/Typed.lean
+++ LogicalRefinement/Typed.lean
@@ -105,12 +105,31 @@ inductive HasType: RawContext -> RawUntyped -> Annot -> Prop
     HasType Γ (eq A l r) prop
 
   -- Basic terms
+  | succ {Γ: RawContext} {n: RawUntyped}:
+    HasType Γ n (expr nats) -> HasType Γ (succ n) (expr nats)
   | lam {Γ: RawContext} {A s B: RawUntyped}:
     HasType ((Hyp.val A)::Γ) s (expr B) ->
     HasType Γ (lam A s) (expr (pi A B))
   | app {Γ: RawContext} {A B l r: RawUntyped}:
     HasType Γ l (expr (pi A B)) -> HasType Γ r (expr A) ->
     HasType Γ (app l r) (expr (B.subst0 l))
+  | pair {Γ: RawContext} {A B l r: RawUntyped}:
+    HasType Γ l (expr A) -> HasType Γ r (expr (B.subst0 l)) ->
+    HasType Γ (pair l r) (expr (sigma A B))
+  | proj_ix {Γ: RawContext} {A B e: RawUntyped}:
+    HasType Γ e (expr (sigma A B)) ->
+    HasType Γ (proj false e) (expr A)
+  | proj_dep {Γ: RawContext} {A B e: RawUntyped}:
+    HasType Γ e (expr (sigma A B)) ->
+    HasType Γ (proj true e) (expr (B.subst0 (proj false e)))
+  | inj_l {Γ: RawContext} {A B e: RawUntyped}:
+    HasType Γ e (expr A) -> HasType Γ B type ->
+    HasType Γ (inj false e) (expr (coprod A B))
+  | inj_r {Γ: RawContext} {A B e: RawUntyped}:
+    HasType Γ e (expr B) -> HasType Γ A type ->
+    HasType Γ (inj true e) (expr (coprod A B))
+  
+    --TODO: rest
 
   -- Basic proofs
   --TODO: this
