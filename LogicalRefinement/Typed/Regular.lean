bebd0b0cfd489d073fabca04f97fbe86ced1c476
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-03-03 11:25:01 +0000

    Toggle sorry

--- LogicalRefinement/Typed/Regular.lean
+++ LogicalRefinement/Typed/Regular.lean
@@ -17,38 +17,38 @@ inductive Annot.regular: Annot -> Context -> Prop
 def Annot.regular_expr: regular (expr s A) Γ -> (Γ ⊢ A: sort s)
   | Annot.regular.expr Hr => Hr
 
-theorem HasType.regular (p: Γ ⊢ a: A): A.regular Γ := by {
-  induction p;
+-- theorem HasType.regular (p: Γ ⊢ a: A): A.regular Γ := by {
+--   induction p;
 
-  -- Types and propositions are trivially regular
-  all_goals try exact Annot.regular.sort
+--   -- Types and propositions are trivially regular
+--   all_goals try exact Annot.regular.sort
 
-  case lam =>
-    constructor; constructor <;>
-    first | assumption | { apply Annot.regular_expr; assumption }
+--   case lam =>
+--     constructor; constructor <;>
+--     first | assumption | { apply Annot.regular_expr; assumption }
 
-  case app =>
-    constructor;
-    apply subst_sort
-    sorry
-    sorry
-    sorry
+--   case app =>
+--     constructor;
+--     apply subst_sort
+--     sorry
+--     sorry
+--     sorry
 
-  --TODO: general tactic for app requires substitution lemma for subst0
+--   --TODO: general tactic for app requires substitution lemma for subst0
 
-  repeat sorry
-}
+--   repeat sorry
+-- }
 
-theorem HasType.term_regular (p: HasType Γ a (term A)): HasType Γ A type 
-  := by {
-    let H := regular p;
-    cases H with
-    | expr H => exact H
-  }
+-- theorem HasType.term_regular (p: HasType Γ a (term A)): HasType Γ A type 
+--   := by {
+--     let H := regular p;
+--     cases H with
+--     | expr H => exact H
+--   }
 
-theorem HasType.proof_regular (p: HasType Γ a (proof A)): HasType Γ A prop 
-  := by {
-    let H := regular p;
-    cases H with
-    | expr H => exact H
-  }
+-- theorem HasType.proof_regular (p: HasType Γ a (proof A)): HasType Γ A prop 
+--   := by {
+--     let H := regular p;
+--     cases H with
+--     | expr H => exact H
+--   }
