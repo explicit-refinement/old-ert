299cdedb8d21a4855edd63e3ce739edb1e85e101
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-09 20:12:13 +0000

    Began interp...

---
+++ LogicalRefinement/Interp.lean
@@ -0,0 +1,5 @@
+import LogicalRefinement.Typed.Basic
+import LogicalRefinement.Typed.Context
+import LogicalRefinement.Typed.Regular
+import LogicalRefinement.Typed.Subst
+import LogicalRefinement.Typed.Wk
