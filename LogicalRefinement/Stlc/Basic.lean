293f6438b2ec5586d30a96cc85e6bbb9013a1c95
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-09 20:47:15 +0000

    begin Stlc HasType

--- LogicalRefinement/Stlc/Basic.lean
+++ LogicalRefinement/Stlc/Basic.lean
@@ -9,9 +9,9 @@ open Ty
 
 inductive Stlc
 -- Basic
+| var (n: Nat)
 | lam (A: Ty) (s: Stlc)
 | app (s t: Stlc)
-| var (n: Nat)
 
 -- Products and coproducts
 | pair (l r: Stlc)
@@ -29,3 +29,13 @@ inductive Stlc
 | zero
 | succ
 | natrec (A: Ty) (n: Stlc) (z: Stlc) (s: Stlc)
+
+def Stlc.Context := List Ty
+
+inductive Stlc.HasVar: Context -> Nat -> Ty -> Prop
+| zero {Γ A}: HasVar (A::Γ) 0 A
+| succ {Γ A B n}: HasVar Γ n A -> HasVar (B::Γ) (Nat.succ n) A
+
+inductive Stlc.HasType: Context -> Stlc -> Ty -> Prop
+| lam {Γ A B s}: HasType (A::Γ) s B -> HasType Γ (lam A s) (fn A B)
+| app {Γ A B s t}: HasType Γ s (fn A B) -> HasType Γ t A -> HasType Γ (app s t) B
