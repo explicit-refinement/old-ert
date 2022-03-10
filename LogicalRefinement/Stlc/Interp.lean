898eece88e9adf2990ab60d874815b2da3e09617
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-10 21:42:23 +0000

    Beginning stlc ty

--- LogicalRefinement/Stlc/Interp.lean
+++ LogicalRefinement/Stlc/Interp.lean
@@ -1,2 +1,8 @@
 import LogicalRefinement.Stlc.Basic
 import LogicalRefinement.Untyped.Basic
+import LogicalRefinement.Typed.Context
+import LogicalRefinement.Typed.Basic
+open Term
+
+def Term.stlc_ty: (a: Term) -> {Γ: Context} -> (p: HasType Γ a type) -> Ty
+| _, _, _ => sorry
