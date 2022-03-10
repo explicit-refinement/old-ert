10d0cef6cad9c4f81d8409ea33ecbe69025d6081
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-10 22:40:55 +0000

    Hmm

--- LogicalRefinement/Stlc/Interp.lean
+++ LogicalRefinement/Stlc/Interp.lean
@@ -6,30 +6,36 @@ import LogicalRefinement.Typed.Regular
 open Term
 open TermKind
 
-def Term.stlc_ty (a: Term) {Γ: Context} (p: HasType Γ a AnnotSort.type): Ty := by {
-  cases a with
-  | var => exact False.elim p.no_poly
-  | const k =>
-    cases k with
-    | unit => exact Ty.unit
-    | nats => exact Ty.nats
-    | _ => apply False.elim; cases p
-  | abs k =>
-    cases k with
-    | pi => sorry
-    | sigma => sorry
-    | assume => sorry
-    | set => sorry
-    | intersect => sorry
-    | union => sorry
-    | _ => apply False.elim; cases p
-  | unary k => cases k <;> apply False.elim <;> cases p
-  | let_bin k => cases k <;> apply False.elim <;> cases p
-  | bin k =>
-    cases k with
-    | coprod => sorry
-    | _ => apply False.elim; cases p
-  | tri k => cases k <;> apply False.elim <;> cases p
-  | cases k => cases k <;> apply False.elim <;> cases p
-  | natrec => apply False.elim; cases p
-}
+-- def Term.stlc_ty (a: Term) {Γ: Context} (p: HasType Γ a AnnotSort.type): Ty := by {
+--   cases a with
+--   | var => exact False.elim p.no_poly
+--   | const k =>
+--     cases k with
+--     | unit => exact Ty.unit
+--     | nats => exact Ty.nats
+--     | _ => apply False.elim; cases p
+--   | abs k A B =>
+--     cases k with
+--     | pi =>
+--       apply Ty.arrow
+--       apply stlc_ty A
+--       cases p; assumption
+--       apply stlc_ty B
+--       cases p; assumption
+--     | sigma => sorry
+--     | assume => sorry
+--     | set => sorry
+--     | intersect => sorry
+--     | union => sorry
+--     | _ => apply False.elim; cases p
+--   | unary k => cases k <;> apply False.elim <;> cases p
+--   | let_bin k => cases k <;> apply False.elim <;> cases p
+--   | bin k =>
+--     cases k with
+--     | coprod => sorry
+--     | _ => apply False.elim; cases p
+--   | tri k => cases k <;> apply False.elim <;> cases p
+--   | cases k => cases k <;> apply False.elim <;> cases p
+--   | natrec => apply False.elim; cases p
+-- } termination_by stlc_ty A _ => A
+--   decreasing_by simp_arith
