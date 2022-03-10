5d0f2d591d2e29cca66392d7e7242a8b832149d1
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-10 21:57:36 +0000

    Pi types

--- LogicalRefinement/Stlc/Interp.lean
+++ LogicalRefinement/Stlc/Interp.lean
@@ -4,7 +4,14 @@ import LogicalRefinement.Typed.Context
 import LogicalRefinement.Typed.Basic
 import LogicalRefinement.Typed.Regular
 open Term
+open TermKind
 
 def Term.stlc_ty: (a: Term) -> {Γ: Context} -> (p: HasType Γ a AnnotSort.type) -> Ty
 | var v, _, H => False.elim H.no_poly
+
+| const TermKind.unit, _, _ => Ty.unit
+| abs TermKind.pi l r, Γ, H => by {
+    apply Ty.arrow (stlc_ty l _) (stlc_ty r _) 
+    <;> (try cases H; assumption)
+  }
 | _, _, _ => sorry
