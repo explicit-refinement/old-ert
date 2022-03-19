ba9a9f050ed396453111d0f7b035fefaef7f3596
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-03-19 03:55:49 +0000

    Filled in Term.stlc_ty

--- LogicalRefinement/Stlc/Interp.lean
+++ LogicalRefinement/Stlc/Interp.lean
@@ -10,8 +10,13 @@ open TermKind
 set_option maxHeartbeats 1000000
 
 def Term.stlc_ty: Term -> Ty
-| var _ => Ty.unit
 | const TermKind.unit => Ty.unit
-| const TermKind.nats => Ty.nats
 | abs TermKind.pi A B => Ty.arrow A.stlc_ty B.stlc_ty
+| abs TermKind.sigma A B => Ty.prod A.stlc_ty B.stlc_ty
+| bin TermKind.coprod A B => Ty.coprod A.stlc_ty B.stlc_ty
+| abs TermKind.assume φ A => A.stlc_ty
+| abs TermKind.set A φ => A.stlc_ty
+| abs TermKind.intersect A B => B.stlc_ty
+| abs TermKind.union A B => B.stlc_ty
+| const TermKind.nats => Ty.nats
 | _ => Ty.bot
