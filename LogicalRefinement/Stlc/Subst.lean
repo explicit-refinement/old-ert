9230c50cb325e1569d960510badc9f54816352e1
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-03-22 17:04:36 +0000

    SubstCtx.interp

--- LogicalRefinement/Stlc/Subst.lean
+++ LogicalRefinement/Stlc/Subst.lean
@@ -3,5 +3,9 @@ import LogicalRefinement.Typed
 import LogicalRefinement.Stlc.Basic
 import LogicalRefinement.Stlc.Interp
 
-def Stlc.Subst.interp (σ: Subst) (Δ: Context): Type :=  
-  ∀{n A}, Stlc.HasVar Δ n A -> A.interp
+def Stlc.Subst.interp (σ: Subst) (Γ Δ: Context): Type :=  
+  ∀{n A}, Stlc.HasVar Δ n A -> Γ.interp -> A.interp
+
+def Stlc.SubstCtx.interp {σ: Subst} {Γ Δ: Context} (S: SubstCtx σ Γ Δ)
+  : σ.interp Γ Δ
+  := sorry
