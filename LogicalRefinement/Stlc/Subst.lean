609ed6434e73d002eabc7aa779460c2ac181ec5b
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-23 20:36:30 +0000

    Hmm

--- LogicalRefinement/Stlc/Subst.lean
+++ LogicalRefinement/Stlc/Subst.lean
@@ -13,9 +13,11 @@ def Stlc.SubstCtx.interp {σ: Subst} {Γ Δ: Context} (S: SubstCtx σ Γ Δ)
   : InterpSubst Γ Δ
   := λHv => (S Hv).interp
   
-def Stlc.InterpSubst.transport_ctx {Γ Δ: Context} (S: InterpSubst Γ Δ)
-  : Γ.interp -> Δ.interp
-  := sorry
+def Stlc.InterpSubst.transport_ctx {Γ Δ: Context} (S: InterpSubst Γ Δ) (G: Γ.interp)
+  : Δ.interp
+  := match Δ with
+     | [] => ()
+     | A::Δ => sorry --((S HasVar.zero G), S.pop.transport_ctx G)
 
 def Stlc.InterpSubst.subst {Γ Δ: Context} (S: InterpSubst Γ Δ):
   ∀{A}, Δ.deriv A -> Γ.deriv A
