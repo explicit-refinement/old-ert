84b12cb2271f246a87cfc17a1eb94731a34ab861
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-23 20:33:43 +0000

    Hmm

--- LogicalRefinement/Stlc/Subst.lean
+++ LogicalRefinement/Stlc/Subst.lean
@@ -3,12 +3,20 @@ import LogicalRefinement.Typed
 import LogicalRefinement.Stlc.Basic
 import LogicalRefinement.Stlc.Interp
 
-def Stlc.InterpSubst (Γ Δ: Context): Type := Γ.interp -> Δ.interp
+def Stlc.InterpSubst (Γ Δ: Context): Type := 
+  ∀{n A}, Stlc.HasVar Δ n A -> Γ.deriv A
 
-def Stlc.InterpSubst.subst {Γ Δ: Context} (S: InterpSubst Γ Δ):
-  ∀{A}, Δ.deriv A -> Γ.deriv A
-  := λD G => D (S G)
+def Stlc.InterpSubst.pop {Γ Δ: Context} (S: InterpSubst Γ (H::Δ)): InterpSubst Γ Δ
+  := λHv => S Hv.succ
 
 def Stlc.SubstCtx.interp {σ: Subst} {Γ Δ: Context} (S: SubstCtx σ Γ Δ)
   : InterpSubst Γ Δ
+  := λHv => (S Hv).interp
+  
+def Stlc.InterpSubst.transport_ctx {Γ Δ: Context} (S: InterpSubst Γ Δ)
+  : Γ.interp -> Δ.interp
   := sorry
+
+def Stlc.InterpSubst.subst {Γ Δ: Context} (S: InterpSubst Γ Δ):
+  ∀{A}, Δ.deriv A -> Γ.deriv A
+  := λD G => D (S.transport_ctx G)
