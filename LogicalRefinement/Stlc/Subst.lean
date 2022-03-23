3ba2c2faae511cade217b7ff5f69f25af8b95271
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-23 20:29:26 +0000

    Different structure...

--- LogicalRefinement/Stlc/Subst.lean
+++ LogicalRefinement/Stlc/Subst.lean
@@ -3,17 +3,12 @@ import LogicalRefinement.Typed
 import LogicalRefinement.Stlc.Basic
 import LogicalRefinement.Stlc.Interp
 
-def Stlc.InterpSubst (Γ Δ: Context): Type := 
-  ∀{n A}, Stlc.HasVar Δ n A -> Γ.deriv A
-
-def Stlc.SubstCtx.interp {σ: Subst} {Γ Δ: Context} (S: SubstCtx σ Γ Δ)
-  : InterpSubst Γ Δ
-  := λHv => (S Hv).interp
-  
-def Stlc.InterpSubst.transport_ctx {Γ Δ: Context} (S: InterpSubst Γ Δ)
-  : Γ.interp -> Δ.interp
-  := sorry
+def Stlc.InterpSubst (Γ Δ: Context): Type := Γ.interp -> Δ.interp
 
 def Stlc.InterpSubst.subst {Γ Δ: Context} (S: InterpSubst Γ Δ):
   ∀{A}, Δ.deriv A -> Γ.deriv A
-  := λD G => D (S.transport_ctx G)
+  := λD G => D (S G)
+
+def Stlc.SubstCtx.interp {σ: Subst} {Γ Δ: Context} (S: SubstCtx σ Γ Δ)
+  : InterpSubst Γ Δ
+  := sorry
