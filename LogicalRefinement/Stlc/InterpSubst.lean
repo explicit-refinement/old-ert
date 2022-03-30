9908267079ea6e8f4f513146657055e0a2641d93
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2022-03-30 11:24:04 +0100

    Added HasType dependency...

--- LogicalRefinement/Stlc/InterpSubst.lean
+++ LogicalRefinement/Stlc/InterpSubst.lean
@@ -4,12 +4,42 @@ import LogicalRefinement.Stlc.Basic
 import LogicalRefinement.Stlc.Interp
 import LogicalRefinement.Stlc.Subst
 
-def Subst.stlc (σ: Subst): Stlc.Subst := sorry
+def Subst.stlc (σ: Subst): Stlc.Subst := 
+  λv => (σ v).stlc
 
 theorem SubstCtx.stlc {σ Γ Δ} (S: SubstCtx σ Γ Δ)
   : Stlc.SubstCtx σ.stlc Γ.stlc Δ.stlc
-  := sorry
+  := by {
+    intro n A Hv;
+    simp only [Subst.stlc]
+    sorry
+  }
 
-theorem Term.subst_stlc_commute {σ: Subst} {a: Term}
+-- But why...
+set_option maxHeartbeats 1000000
+
+theorem Term.subst_stlc_commute {σ a} (H: HasType Γ a A)
   : (a.subst σ).stlc = a.stlc.subst σ.stlc
-  := sorry
+  := by {
+    revert σ;
+    induction H with
+    | lam => sorry
+    | app => sorry
+    | pair => sorry
+    | let_pair => sorry
+    | inj_l => sorry
+    | inj_r => sorry
+    | case => sorry
+    | elem => sorry
+    | let_set => sorry
+    | lam_pr => sorry
+    | app_pr => sorry
+    | lam_irrel => sorry
+    | app_irrel => sorry
+    | repr => sorry
+    | let_repr => sorry
+    | natrec => sorry
+    | _ => 
+      intro σ;
+      rfl
+  }
