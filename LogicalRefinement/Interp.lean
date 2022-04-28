81c2b032f3961bf77c70d5e58deb805f150ab0fe
Jad Ghalayini <jad.ghalayini@protonmail.com>
2022-04-28 03:33:37 +0100

    Stlc module

--- LogicalRefinement/Interp.lean
+++
@@ -1,5 +0,0 @@
-import LogicalRefinement.Typed.Basic
-import LogicalRefinement.Typed.Context
-import LogicalRefinement.Typed.Regular
-import LogicalRefinement.Typed.Subst
-import LogicalRefinement.Typed.Wk
---
+++ LogicalRefinement/Stlc.lean
@@ -0,0 +1,7 @@
+prelude
+import LogicalRefinement.Stlc.Basic
+import LogicalRefinement.Stlc.Interp
+import LogicalRefinement.Stlc.InterpInv
+import LogicalRefinement.Stlc.InterpSubst
+import LogicalRefinement.Stlc.InterpWk
+import LogicalRefinement.Stlc.Subst
